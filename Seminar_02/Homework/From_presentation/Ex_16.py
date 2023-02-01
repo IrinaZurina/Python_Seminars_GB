bf49db6290e7a6e7f9e156256c08deaefc220fd8
IrinaZurina <zurinai@yandex.ru>
2023-02-01 11:19:43 +0300

    Task 16 updated with var2

--- Seminar_02/Homework/From_presentation/Ex_16.py
+++ Seminar_02/Homework/From_presentation/Ex_16.py
@@ -1,6 +1,7 @@
 """Требуется вывести все целые степени двойки (т.е. числа
 вида 2k ), не превосходящие числа N.
 """
+# var1 без списка
 user_input = int(input('Введите целое положительное число: '))
 two_pow = 1
 power = 1
@@ -9,3 +10,13 @@ while two_pow <= user_input:
     two_pow = 2 ** power
     power += 1
 
+# var2 со списком
+user_input = int(input('Введите целое положительное число: '))
+two_pow_list = []
+two_pow = 1
+power = 1
+while two_pow <= user_input:
+    two_pow_list.append(two_pow)
+    two_pow = 2 ** power
+    power += 1
+print(*two_pow_list)

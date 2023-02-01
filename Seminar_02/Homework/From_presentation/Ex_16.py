c0a06ee2d9163900750f9f6c9a46c196a971032e
IrinaZurina <zurinai@yandex.ru>
2023-02-01 11:21:01 +0300

    Task 16 update

--- Seminar_02/Homework/From_presentation/Ex_16.py
+++ Seminar_02/Homework/From_presentation/Ex_16.py
@@ -3,8 +3,7 @@
 """
 # var1 без списка
 user_input = int(input('Введите целое положительное число: '))
-two_pow = 1
-power = 1
+two_pow, power = 1, 1
 while two_pow <= user_input:
     print(two_pow, end=' ')
     two_pow = 2 ** power
@@ -13,8 +12,7 @@ while two_pow <= user_input:
 # var2 со списком
 user_input = int(input('Введите целое положительное число: '))
 two_pow_list = []
-two_pow = 1
-power = 1
+two_pow, power = 1, 1
 while two_pow <= user_input:
     two_pow_list.append(two_pow)
     two_pow = 2 ** power

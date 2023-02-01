d57bcac716bbb4b83e8a65a216c7ca242e2cd80a
IrinaZurina <zurinai@yandex.ru>
2023-02-01 09:40:21 +0300

    Task 4 from site complete

--- Seminar_02/Homework/From_site/Ex_04.py
+++ Seminar_02/Homework/From_site/Ex_04.py
@@ -2,5 +2,11 @@
 Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число."""
 
 from random import randint
-user_input = int(input('Введите целое положительное число'))
+user_input = int(input('Введите целое положительное число: '))
 elements_list = [randint(-user_input, user_input) for _ in range(user_input)]
+indexes_file = open('Ex_4.txt', 'r')
+indexes = indexes_file.readlines()
+indexes_file.close()
+product = elements_list[int(indexes[0].rstrip())] * elements_list[int(indexes[1].rstrip())]
+print(*elements_list)
+print(product)

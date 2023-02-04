8438a4eb7c9005914b79c60f6dd6d7573a91661d
IrinaZurina <zurinai@yandex.ru>
2023-02-04 07:48:33 +0300

    Homework update

--- Seminar_03/Classwork/Ex_21.py
+++ Seminar_03/Classwork/Ex_21.py
@@ -3,10 +3,10 @@ Input: [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
 {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII
 ":" S007 "}]
 """
-user_dict = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": "S005"},
-             {"V":"S009"}, {" VIII": "S007"}]
+user_dict = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": " S005 "}, {"VII": "S005"},
+             {"V":" S009 "}, {" VIII": "S007"}]
 values = set()
 for i in range(len(user_dict)):
     for value in user_dict[i].values():
-        values.add(user_dict[i][key])
+        values.add(value.strip())
 print(f'Множество уникальных значений: {values}')

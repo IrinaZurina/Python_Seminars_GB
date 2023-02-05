3f0b43841661e6338a9932a6c6eee963849b9985
Irina Zurina <zurinai@yandex.ru>
2023-02-05 16:20:36 +0300

    Homework from presentation solved

---
+++ Seminar_04/Homework/From_presentation/Ex_22.py
@@ -0,0 +1,12 @@
+"""Задача 22: Даны два неупорядоченных набора целых чисел (может быть, с
+повторениями). Выдать без повторений в порядке возрастания все те числа, которые
+встречаются в обоих наборах.
+Пользователь вводит 2 числа. n - кол-во элементов первого множества. m - кол-во
+элементов второго множества. Затем пользователь вводит сами элементы множеств.
+"""
+from random import randint
+n, m = [int(input()) for _ in range(2)]
+first_set = set(randint(1, 100) for _ in range(n))
+second_set = set(randint(1, 100) for _ in range(m))
+print(first_set, second_set, sep='\n')
+print(*sorted(first_set.intersection(second_set)))
